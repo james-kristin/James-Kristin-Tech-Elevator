63d5a5a1452616683bd88e6bbdf27eb88415ade0
James Kristin <james.kristin2000@gmail.com>
2022-09-28 16:13:25 -0400

    Completed Exercise_Managing_Inheritance

---
+++ java/module-1/week-6/03_Managing_Inheritance/exercise/src/main/java/com/techelevator/RectangleWall.java
@@ -0,0 +1,33 @@
+package com.techelevator;
+
+import java.sql.SQLOutput;
+
+public class RectangleWall extends Wall{
+
+    private int length;
+    private int height;
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public RectangleWall(String name, String color, int length, int height) {
+        super(name, color);
+        this.length = length;
+        this.height = height;
+    }
+
+    @Override
+    public int getArea() {
+        return length * height;
+    }
+
+    @Override
+    public String toString() {
+        return super.getName() + " (" + length + "x" + height + ") rectangle";
+    }
+}
