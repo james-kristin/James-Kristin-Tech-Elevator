8ff6578d363e8780af0ea3f637a4f9bda39de1a6
James Kristin <james.kristin2000@gmail.com>
2022-10-13 22:13:19 -0400

    Completed Exercise_FindAndReplace_FizzWriter

--- java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,9 +1,46 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
 
+        System.out.println("What is the search word?");
+        String searchWord = userInput.nextLine();
+
+        System.out.println("What is the replacement word?");
+        String replacementWord = userInput.nextLine();
+
+        System.out.println("What is the source file?");
+        String sourceFilePath = userInput.nextLine();
+
+        System.out.println("What is the destination file?");
+        String destinationFilePath = userInput.nextLine();
+
+        File sourceFile = new File(sourceFilePath);
+        File destinationFile = new File(destinationFilePath);
+
+        try (Scanner sourceInput = new Scanner(sourceFile);
+             PrintWriter destinationWriter = new PrintWriter(destinationFile)) {
+
+                while (sourceInput.hasNextLine()) {
+                    String lineOfText =sourceInput.nextLine();
+
+                    if (lineOfText.contains(searchWord)) {
+                        destinationWriter.println(lineOfText.replace(searchWord, replacementWord));
+                    } else {
+                        destinationWriter.println(lineOfText);
+                    }
+                }
+        } catch (FileNotFoundException e) {
+            System.out.println("One or more file paths could not be found" + sourceFile.getAbsolutePath() + " or " + destinationFile.getAbsolutePath());
+            return;
+        }
     }
 
 }
--- java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,36 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FizzWriter {
 
 	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
 
+		System.out.println("What is the destination File?");
+		String destinationFilePath = userInput.nextLine();
+
+		File destinationFile = new File(destinationFilePath);
+
+		try (PrintWriter destinationWriter = new PrintWriter(destinationFile)) {
+			for (int i = 1; i <= 300; i++) {
+				if ((i % 3 == 0) && (i % 5 == 0)) {
+					destinationWriter.println("FizzBuzz");
+				} else if (i % 3 == 0) {
+					destinationWriter.println("Fizz");
+				} else if (i % 5 == 0) {
+					destinationWriter.println("Buzz");
+				} else {
+					destinationWriter.println(i);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("The destination file path could not be found: " + destinationFile.getAbsolutePath());
+			return;
+		}
 	}
 
 }
