64bad939c0a473a2b370b1bdbc46111a78afe7f7
James Kristin <james.kristin2000@gmail.com>
2022-10-21 13:18:38 -0400

    FileSplitter_Partial

--- java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FileSplitter.java
+++ java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FileSplitter.java
@@ -1,9 +1,40 @@
 package com.techelevator;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class FileSplitter {
 
 	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
 
+		System.out.println("What is the file path of the input file?");
+		String inputFilePath = userInput.nextLine();
+
+		System.out.println("How many lines of text (max) should there be in the split files?");
+		String maxTextLines = userInput.nextLine();
+
+		File inputFile = new File(inputFilePath);
+		System.out.println(inputFile);
+		File convertedInputFile = convertInputFile(inputFile, 1);
+
+
+
+	}
+	static private File convertInputFile(File inputFile, int timesSplit) {
+		String inputPath = inputFile.getAbsolutePath();
+		int dotIndex = inputPath.lastIndexOf('.');
+
+		String convertedPath;
+
+		if (dotIndex >= 0) {
+			convertedPath = inputPath.substring(0, dotIndex) + "-" + timesSplit + inputPath.substring(dotIndex, +1);
+
+		} else {
+			convertedPath = inputPath + "-" + timesSplit;
+
+		}
+		return new File(convertedPath);
 	}
 
 }
